d7ca4696392d280bd58ece3da6543beb937f44b3
Melissa Ip <nokami.hybrids@gmail.com>
2013-12-08 21:46:08 -0700

    refactored count_bin.py

--- count_bin.py
+++ count_bin.py
@@ -9,6 +9,7 @@ This script creates bins with probability cateogories in percentage
 the sites in each bin that contain a real mutation, meaning the value in
 the has mutation column is 1.
 """
+import math
 import sys
 
 BIN_SIZE = 10
@@ -23,46 +24,11 @@ for line in lines:
     values = line.strip('\n').split('\t')
     proba = float(values[0])
     has_muta = int(values[1])
-    if proba >= 0.9:
-        total_counts[9] += 1
-        if has_muta == 1:
-            counts[9] += 1
-    elif proba >= 0.8 and proba < 0.9:
-        total_counts[8] += 1
-        if has_muta == 1:
-            counts[8] += 1
-    elif proba >= 0.7 and proba < 0.8:
-        total_counts[7] += 1
-        if has_muta == 1:
-            counts[7] += 1
-    elif proba >= 0.6 and proba < 0.7:
-        total_counts[6] += 1
-        if has_muta == 1:
-            counts[6] += 1
-    elif proba >= 0.5 and proba < 0.6:
-        total_counts[5] += 1
-        if has_muta == 1:
-            counts[5] += 1
-    elif proba >= 0.4 and proba < 0.5:
-        total_counts[4] += 1
-        if has_muta == 1:
-            counts[4] += 1
-    elif proba >= 0.3 and proba < 0.4:
-        total_counts[3] += 1
-        if has_muta == 1:
-            counts[3] += 1
-    elif proba >= 0.2 and proba < 0.3:
-        total_counts[2] += 1
-        if has_muta == 1:
-            counts[2] += 1
-    elif proba >= 0.1 and proba < 0.2:
-        total_counts[1] += 1
-        if has_muta == 1:
-            counts[1] += 1
-    elif proba < 0.1:
-        total_counts[0] += 1
-        if has_muta == 1:
-            counts[0] += 1
+
+    bin = int(min(math.floor(proba * 10), 9))
+    total_counts[bin] += 1
+    if has_muta == 1:
+        counts[bin] += 1
 fin.close()
 
 for x in range(BIN_SIZE):
